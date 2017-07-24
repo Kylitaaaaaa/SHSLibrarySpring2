056e484a77cc3f1a9da6c4cb037d4205f69fa3c0
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2017-07-24 17:25:20 +0800

    Updated Lib Staff and Lib Manager Controller
    Working:
    Add
    -book
    -customer
    -admin
    
    Edit
    -book
    -customer
    -admin
    
    Delete
    -book
    
    Get all:
    -libstaff
    -libman
    -book
    -customer
    -meetingroom
    -review
    -user
    -admin
    
    Others
    -Unlock and Lock Account

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -31,6 +31,35 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping(path="/allBook")
+    public @ResponseBody Iterable<Book> getAllBook() {
+        // This returns a JSON or XML with the users
+        return bookRepository.findAll();
+    }
+
+    @GetMapping(path="/allCustomer")
+    public @ResponseBody Iterable<Customer> getAllCustomer() {
+        // This returns a JSON or XML with the users
+        return customerRepository.findAll();
+    }
+
+    @GetMapping(path="/allMeetingRoom")
+    public @ResponseBody Iterable<Meeting_Room> getAllMeetingRoom() {
+        // This returns a JSON or XML with the users
+        return meeting_roomRepository.findAll();
+    }
+
+    @GetMapping(path="/allReview")
+    public @ResponseBody Iterable<Review> getAllReview() {
+        // This returns a JSON or XML with the users
+        return reviewRepository.findAll();
+    }
+
+    @GetMapping(path="/allUser")
+    public @ResponseBody Iterable<User> getAllUser() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
 
     @GetMapping(path="/allAdmin")
     public @ResponseBody Iterable<Admin> getAllAdmin() {
@@ -70,6 +99,46 @@ public class MainController {
                 users.remove(i);
 
         return users;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/addCustomer")
+    public @ResponseBody String addNewCustomer (@RequestParam String first_name,
+                                             @RequestParam String middle_initial,
+                                             @RequestParam String last_name,
+                                             @RequestParam String birthday,
+                                             @RequestParam String secret_question,
+                                             @RequestParam String secret_answer,
+                                             @RequestParam int id_number,
+                                             @RequestParam String password,
+                                             @RequestParam int user_type,
+                                             @RequestParam String email_address,
+                                             @RequestParam String phone_number) {
+
+        Customer c = new Customer();
+        c.setFirst_name(first_name);
+        c.setMiddle_initial(middle_initial);
+        c.setLast_name(last_name);
+        c.setBirthday(birthday);
+        c.setSecret_question(secret_question);
+        c.setSecret_answer(secret_answer);
+        c.setCustomer_type(user_type);
+        customerRepository.save(c);
+
+        User u = new User();
+        u.setId_number(id_number);
+        u.setPassword(password);
+        u.setEmail_address(email_address);
+        u.setPhone_number(phone_number);
+        u.setLock_status(0);
+        u.setLogin_attempts(0);
+
+        if(user_type == 1)
+            u.setUser_type(1);
+        else if(user_type == 2)
+            u.setUser_type(2);
+        userRepository.save(u);
+
+        return "Customer Saved";
     }
 
 
@@ -113,6 +182,70 @@ public class MainController {
         return "Saved";
     }
 
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/addBook")
+    public @ResponseBody String addNewBook (@RequestParam String title,
+                                             @RequestParam int type,
+                                             @RequestParam String author,
+                                             @RequestParam String publisher,
+                                             @RequestParam int year,
+                                             @RequestParam String location,
+                                             @RequestParam int status) {
+
+        Book b = new Book();
+        b.setTitle(title);
+        b.setType(type);
+        b.setAuthor(author);
+        b.setPublisher(publisher);
+        b.setYear(year);
+        b.setLocation(location);
+        b.setStatus(status);
+
+        bookRepository.save(b);
+        return "Book Saved";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/updateAdmin")
+    public @ResponseBody String updateCustomer (@RequestParam int customer_id,
+                                             @RequestParam String first_name,
+                                             @RequestParam String middle_initial,
+                                             @RequestParam String last_name,
+                                             @RequestParam String birthday,
+                                             @RequestParam String secret_question,
+                                             @RequestParam String secret_answer) {
+
+        Customer c = customerRepository.findOne(customer_id);
+
+        c.setFirst_name(first_name);
+        c.setMiddle_initial(middle_initial);
+        c.setLast_name(last_name);
+        c.setBirthday(birthday);
+        c.setSecret_question(secret_question);
+        c.setSecret_answer(secret_answer);
+        customerRepository.save(c);
+
+        return "cru";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/updateAdmin")
+    public @ResponseBody String updateUser (@RequestParam int user_id,
+                                                @RequestParam int id_number,
+                                                @RequestParam String password,
+                                                @RequestParam int user_type,
+                                                @RequestParam String email_address,
+                                                @RequestParam String phone_number) {
+        User u = userRepository.findOne(user_id);
+
+        u.setId_number(id_number);
+        u.setPassword(password);
+        u.setUser_type(user_type);
+        u.setEmail_address(email_address);
+        u.setPhone_number(phone_number);
+
+        userRepository.save(u);
+
+        return "User Updated";
+    }
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/updateAdmin")
     public @ResponseBody String updateAdmin (@RequestParam int admin_id,
                                              @RequestParam String first_name,
@@ -134,6 +267,42 @@ public class MainController {
 
         return "cru";
     }
+
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/updateBook")
+    public @ResponseBody String updateBook (@RequestParam int book_id,
+                                            @RequestParam String title,
+                                            @RequestParam int type,
+                                            @RequestParam String author,
+                                            @RequestParam String publisher,
+                                            @RequestParam int year,
+                                            @RequestParam String location,
+                                            @RequestParam int status) {
+
+        Book b = bookRepository.findOne(book_id);
+        b.setTitle(title);
+        b.setType(type);
+        b.setAuthor(author);
+        b.setPublisher(publisher);
+        b.setYear(year);
+        b.setLocation(location);
+        b.setStatus(status);
+
+        bookRepository.save(b);
+
+        return "Book Updated";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/deleteBook")
+    public @ResponseBody String deleteBook (@RequestParam int book_id) {
+
+        bookRepository.delete(book_id);
+
+        return "Book Deleted";
+    }
+
+
+
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/unlockAccount")
     public @ResponseBody String unlockAccount (@RequestParam int user_id) {
@@ -166,59 +335,9 @@ public class MainController {
 
 
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, path="/addUser")
-    public @ResponseBody String addNewUser (@RequestParam String id_number,
-                                             @RequestParam String password,
-                                             @RequestParam String user_type,
-                                             @RequestParam String email_address,
-                                             @RequestParam String phone_number) {
 
-        User u = new User();
-        u.setId_number(Integer.parseInt(id_number));
-        u.setPassword(password);
-        u.setEmail_address(email_address);
-        u.setPhone_number(phone_number);
-        u.setLock_status(0);
-        u.setLogin_attempts(0);
-        u.setUser_type(Integer.parseInt(user_type));
 
-//        if(user_type == 1)
-//            u.setUser_type(1);
-//        else if(user_type == 2)
-//            u.setUser_type(2);
-        userRepository.save(u);
-        return "Saved";
-    }
 
-    @GetMapping(path="/allBook")
-    public @ResponseBody Iterable<Book> getAllBook() {
-        // This returns a JSON or XML with the users
-        return bookRepository.findAll();
-    }
-
-    @GetMapping(path="/allCustomer")
-    public @ResponseBody Iterable<Customer> getAllCustomer() {
-        // This returns a JSON or XML with the users
-        return customerRepository.findAll();
-    }
-
-    @GetMapping(path="/allMeetingRoom")
-    public @ResponseBody Iterable<Meeting_Room> getAllMeetingRoom() {
-        // This returns a JSON or XML with the users
-        return meeting_roomRepository.findAll();
-    }
-
-    @GetMapping(path="/allReview")
-    public @ResponseBody Iterable<Review> getAllReview() {
-        // This returns a JSON or XML with the users
-        return reviewRepository.findAll();
-    }
-
-    @GetMapping(path="/allUser")
-    public @ResponseBody Iterable<User> getAllUser() {
-        // This returns a JSON or XML with the users
-        return userRepository.findAll();
-    }
 
 
 }
