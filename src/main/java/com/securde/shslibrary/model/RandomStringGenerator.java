f8fe815d0ef898de872e584b0d5e58f2336a3322
Benson Polican <benson_polican@dlsu.edu.ph>
2017-08-10 21:32:43 +0800

    updated

--- src/main/java/com/securde/shslibrary/model/RandomStringGenerator.java
+++ src/main/java/com/securde/shslibrary/model/RandomStringGenerator.java
@@ -13,12 +13,16 @@ public class RandomStringGenerator {
     public String genString(int length){
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
+        String specialChars = "*$-+?_&=!%{}/";
+        String lCaseChars = "abcdefgijkmnopqrstwxyz";
         Random rnd = new Random();
         while (salt.length() < length) { // length of the random string.
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
         String saltStr = salt.toString();
+        saltStr.concat(specialChars.charAt(rnd.nextInt(specialChars.length()))+" "+lCaseChars.charAt(rnd.nextInt(lCaseChars.length())));
+
         return saltStr;
     }
 }
